19ba073d4fc49d04250bdcef9d4ab4fa2501b57a
John Wood <john@kolon.co.uk>
2017-12-10 17:38:47 +0000

    various updates

--- upd-gitignore.sh
+++ upd-gitignore.sh
@@ -1,1 +1,7 @@
+#get new .gitignore from github
 wget -q https://raw.githubusercontent.com/github/gitignore/master/Python.gitignore -O .gitignore
+
+#don't forget to ignore our own config file!
+echo  >> .gitignore
+echo \# conclamantes config file >> .gitignore
+echo  config.ini >> .gitignore
